6100b00da555bdaf654a196d64e7ae2a55363bbf
vsitaraman <vs19g@fsu.edu>
2026-05-01 23:30:09 -0400

    modified:   talkmap/map.html 	modified:   talkmap/org-locations.js 	modified:   talkmap_out.ipynb

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,32 @@
 var addressPoints = [
   [
-    "Talk 2 on Relevant Topic in Your Field<br />London School of Testing; London, UK",
-    51.4893335,
-    -0.14405508452768728
+    "Solar Water Desalination using Nanophotonics<br />BITS Pilani - Hyderabad Campus; Hyderabad, India",
+    17.360589,
+    78.4740613
   ],
   [
-    "Tutorial 1 on Relevant Topic in Your Field<br />UC-Berkeley Institute for Testing Science; Berkeley, CA, USA",
-    37.8708393,
-    -122.272863
+    "Neutron Star Workshop<br />BITS Pilani - Hyderabad Campus; Hyderabad, India",
+    17.360589,
+    78.4740613
   ],
   [
-    "Conference Proceeding talk 3 on Relevant Topic in Your Field<br />Testing Institute of America 2014 Annual Conference; Los Angeles, CA, USA",
-    34.0536909,
-    -118.242766
+    "Upgrades to the ANASEN Active Target Detector for (alpha,p) reactions with radioactive beams<br />Gordon Research Conference for Nuclear Chemistry; New London, New Hampshire",
+    43.414026,
+    -71.985168
   ],
   [
-    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
-    37.7792588,
-    -122.4193286
+    "The study of the 17 F (\u03b1,p) 20 Ne reaction rate using the ANASEN Active target detector<br />APS Division of Nuclear Physics (DNP) Fall Meeting; Boston, MA",
+    42.3588336,
+    -71.0578303
+  ],
+  [
+    "PlasmaSurf Summer School for Nuclear Fusion and Plasma Physics (ITER/IPFN)<br />Instituto de Plasmas e Fus\u00e3o Nuclear (IPFN); Lisbon, Portugal",
+    38.7077507,
+    -9.1365919
+  ],
+  [
+    "The study of the 27 Al (\u03b1,p) reaction using the upgraded ANASEN detector<br />APS Division of Nuclear Physics (DNP) Fall Meeting; Boston, MA",
+    42.3588336,
+    -71.0578303
   ]
 ];
